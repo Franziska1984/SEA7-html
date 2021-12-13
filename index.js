ab62d3f3c835c74d7de4508dc90ea9d157ddb136
Franziska1984 <franziska.wienecke@gmx.de>
2021-12-13 16:19:42 +0100

    b1: js b

--- index.js
+++ index.js
@@ -8,3 +8,31 @@ console.log("Hallo Welt!")
 /* Das ist auch ein Kommentar
  *
 */
+
+function faculty( zahl ) {
+      var n = 1; // Zum hochzaehlen
+      var m = 1; // Ergebnis (Fakultät)
+      while ( n <= zahl ) {
+	   m = m * n;
+	   n = n + 1;
+	console.log(m);
+      }
+      return m;
+}
+
+function ausrechnen() {
+     var eingabe = document.getElementById("eingabefeld").value;
+     	
+     var ausgabe = faculty(eingabe);
+     document.getElementById("ausgabefeld").value = ausgabe;
+}
+/*ausrechnen();*/
+
+document.getElementById("Rechner").addEventListener("click",ausrechnen);
+
+
+
+
+
+
+
